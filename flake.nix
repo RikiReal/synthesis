571e6835fd1eb342eaabe598b3fd76f69d1af761
RikiReal <rijad@tutanota.de>
2026-04-30 22:07:30 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Synthesis: A complete and functional NixOS configuration for my personal setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/desktop/configuration.nix
+        ];
+      };
+    };
+}
